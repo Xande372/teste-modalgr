e51a0865190640375cfcab1128d188474bfb4063
Alexandre Direito <xandedsj@gmail.com>
2025-02-02 12:48:16 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # teste-modalgr
+
+De acordo com a imagem anexada no email:
+
+a) Acredito que serão necessário pelo menos 9 testes:
+ 
+ b)
+    1- validacaoNome;
+    2- validacaoCPF;
+    3- validacaoEmail;
+    4- validacaoSenha;
+    5- validacaoConfirmarSenha;
+    6- cadastroBemSucedido;
+    7- tratamento de erros com Try e catch;
+    8- prevencaoXSS;
+    9- prevencaoSQLI.
---
+++ index.js
@@ -0,0 +1,35 @@
+function calcularMulta(valorPorDia, diasAtraso) {
+    var valorMulta = valorPorDia * diasAtraso;
+
+    // valor mínimo da multa
+    if (valorMulta < 1) {
+        valorMulta = 1;
+    }
+    
+
+    return `O valor da multa para ${diasAtraso} dias de atraso é: R$${valorMulta}`;
+}
+
+// Exemplo de uso no CLI
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+readline.question('Digite o valor da multa por dia (em R$): ', (valorPorDia) => {
+    readline.question('Digite o número de dias de atraso: ', (diasAtraso) => {
+        
+        valorPorDia = parseFloat(valorPorDia);
+        diasAtraso = parseInt(diasAtraso);
+
+        // Verifica se os valores são válidos
+        if (isNaN(valorPorDia) || isNaN(diasAtraso) || valorPorDia < 0 || diasAtraso < 0) {
+            console.log("Por favor, insira valores válidos.");
+        } else {
+            const mensagem = calcularMulta(valorPorDia, diasAtraso);
+            console.log(mensagem);
+        }
+
+        readline.close();
+    });
+});
